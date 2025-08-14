85e26e41fdc1f1f6f37285271accad41730fae49
Alec Nono <alecbnono@gmail.com>
2025-08-14 23:09:27 +0800

    Temporary fix

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,13 +6,13 @@ function Contact() {
     <section className="flex flex-col gap-20 py-20">
       <h1 className="font-bold text-6xl">Contact</h1>
       <div className="flex flex-col gap-5">
-        <div className="h-75 flex flex-col md:flex-row gap-5 justify-center md:gap-15 rounded-xl border-2 border-white p-5 items-center bg-slate-950/50">
+        <div className="md:h-75 flex flex-col md:flex-row gap-5 justify-center md:gap-15 rounded-xl border-2 border-white p-5 items-center bg-slate-950/50">
           <img
             src="/about/profile-picture.png"
             alt=""
-            className="shrink rounded-full h-full mx-20 md:mx-0 aspect-square"
+            className="max-w-40 max-h-40 md:max-h-65 md:max-w-65 flex-1 rounded-full mx-20 md:mx-0 aspect-square"
           />
-          <div className="text-lg text-slate-400 text-center md:text-left flex-1">
+          <div className="text-lg text-slate-400 text-center md:text-left">
             <h1 className="font-bold text-5xl text-slate-50">Alec Nono</h1>
             <p>Computer Science Student</p>
             <p>Software Engineer</p>
