f59ba41e8fe417cf1ee390e509633f268b9758f7
Alec Nono <alecbnono@gmail.com>
2025-08-27 14:45:25 +0800

    Update org end date

--- src/data/orgs.js
+++ src/data/orgs.js
@@ -1,28 +1,28 @@
 const orgs = [
   {
     dateStart: "Feb 2025",
-    dateEnd: "Present",
+    dateEnd: "Aug 2025",
     title: "Web Development Executive",
     description: "Computer Studies Government, De La Salle University",
     image: "/orgs/csg.jpg",
   },
   {
     dateStart: "Feb 2025",
-    dateEnd: "Present",
+    dateEnd: "Aug 2025",
     title: "Research and Development Executive",
     description: "CATCH2T28",
     image: "/orgs/catch.jpg",
   },
   {
     dateStart: "Nov 2024",
-    dateEnd: "Present",
+    dateEnd: "Aug 2025",
     title: "Developer",
     description: "Google Developer Groups on Campus - DLSU",
     image: "/orgs/gdsc.png",
   },
   {
     dateStart: "Nov 2024",
-    dateEnd: "Present",
+    dateEnd: "Aug 2025",
     title: "Research and Development Executive",
     description: "DLSU Microsoft Student Community",
     image: "/orgs/msc.png",
